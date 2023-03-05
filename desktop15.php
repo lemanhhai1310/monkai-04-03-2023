a0dc768ad911bbd6e157232c4bcc27fca54e14c4
hailm <hai.lemanh.1310@gmail.com>
2023-03-05 23:56:26 +0700

    page desktop15

---
+++ desktop15.php
@@ -0,0 +1,46 @@
+<?php $data["title"] = "Desktop 15"; ?>
+<?php $isLogin = true; ?>
+<?php $isBack = true; ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-container">
+    <h2 class="uk-h2 desktop12__title uk-text-center uk-margin-remove">Payment history</h2>
+    <div class="desktop12__desc uk-text-center item__24-12">Choose a plan that works for you.</div>
+    <div class="item__68-40 uk-child-width-1-3@m uk-grid-small uk-grid-32-m uk-grid-match" uk-grid>
+        <?php
+        $data = array(
+            array(
+                'txt1' => '100',
+                'txt2' => '2.99',
+                'txt3' => '0.03',
+            ),
+            array(
+                'txt1' => '200',
+                'txt2' => '4.99',
+                'txt3' => '0.025',
+            ),
+            array(
+                'txt1' => '500',
+                'txt2' => '9.99',
+                'txt3' => '0.02',
+            ),
+            array(
+                'txt1' => '1000',
+                'txt2' => '14.99',
+                'txt3' => '0.015',
+            ),
+        );
+        foreach ($data as $k=>$v): ?>
+        <div>
+            <div class="uk-card desktop12__card uk-card-body uk-padding-small">
+                <div class="desktop12__card__txt1"><?= $v['txt1'] ?> <img src="images/desktop12/Asset11.png" alt=""></div>
+                <div class="item__16 desktop12__card__txt2"><?= $v['txt2'] ?>$</div>
+                <div class="desktop12__card__txt3">22/2/2023</div>
+                <div class="item__24-12">
+                    <a href="" class="desktop15__btn uk-width uk-button uk-button-default uk-button-small uk-border-rounded"><span>more details</span></a>
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
