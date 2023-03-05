d3db3e68e635a6c6b87f62e72c5fee8c8a20f6c7
hailm <hai.lemanh.1310@gmail.com>
2023-03-05 21:25:11 +0700

    page desktop5

---
+++ desktop5.php
@@ -0,0 +1,178 @@
+<?php $data["title"] = "Desktop 5"; ?>
+<?php $isLogin = true; ?>
+<?php $isBack = true; ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-container">
+    <div uk-grid>
+        <div class="uk-width-expand">
+            <h2 class="uk-h2 desktop6__title">Week 12</h2>
+        </div>
+        <div class="uk-width-auto">
+            <div class="uk-child-width-auto uk-grid-24" uk-grid>
+                <div>
+                    <a href="" class="desktop6__iconTab uk-active">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <mask id="mask0_5_1774" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                                <rect width="24" height="24" fill="currentColor"/>
+                            </mask>
+                            <g mask="url(#mask0_5_1774)">
+                                <path d="M4 11V19H8V11H4ZM10 5V19H14V5H10ZM16 13V19H20V13H16ZM20 21H4C3.45 21 2.97917 20.8042 2.5875 20.4125C2.19583 20.0208 2 19.55 2 19V11C2 10.45 2.19583 9.97917 2.5875 9.5875C2.97917 9.19583 3.45 9 4 9H8V5C8 4.45 8.19583 3.97917 8.5875 3.5875C8.97917 3.19583 9.45 3 10 3H14C14.55 3 15.0208 3.19583 15.4125 3.5875C15.8042 3.97917 16 4.45 16 5V11H20C20.55 11 21.0208 11.1958 21.4125 11.5875C21.8042 11.9792 22 12.45 22 13V19C22 19.55 21.8042 20.0208 21.4125 20.4125C21.0208 20.8042 20.55 21 20 21Z" fill="currentColor"/>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+                <div>
+                    <a href="" class="desktop6__iconTab">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <mask id="mask0_5_1266" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                                <rect width="24" height="24" fill="currentColor"/>
+                            </mask>
+                            <g mask="url(#mask0_5_1266)">
+                                <path d="M12 22C9.76667 22 7.875 21.225 6.325 19.675C4.775 18.125 4 16.2333 4 14C4 12.1167 4.55833 10.3083 5.675 8.57501C6.79167 6.84167 8.325 5.32501 10.275 4.02501C10.6417 3.77501 11.0208 3.76251 11.4125 3.98751C11.8042 4.21251 12 4.55001 12 5.00001V6.30001C12 6.86667 12.1958 7.34167 12.5875 7.72501C12.9792 8.10834 13.4583 8.30001 14.025 8.30001C14.3083 8.30001 14.5792 8.23751 14.8375 8.11251C15.0958 7.98751 15.325 7.80834 15.525 7.57501C15.6583 7.40834 15.8292 7.30417 16.0375 7.26251C16.2458 7.22084 16.4417 7.26667 16.625 7.40001C17.675 8.15001 18.5 9.10834 19.1 10.275C19.7 11.4417 20 12.6833 20 14C20 16.2333 19.225 18.125 17.675 19.675C16.125 21.225 14.2333 22 12 22ZM6 14C6 14.8667 6.175 15.6875 6.525 16.4625C6.875 17.2375 7.375 17.9167 8.025 18.5C8.00833 18.4167 8 18.3417 8 18.275V18.05C8 17.5167 8.1 17.0167 8.3 16.55C8.5 16.0833 8.79167 15.6583 9.175 15.275L12 12.5L14.825 15.275C15.2083 15.6583 15.5 16.0833 15.7 16.55C15.9 17.0167 16 17.5167 16 18.05V18.275C16 18.3417 15.9917 18.4167 15.975 18.5C16.625 17.9167 17.125 17.2375 17.475 16.4625C17.825 15.6875 18 14.8667 18 14C18 13.1667 17.8458 12.3792 17.5375 11.6375C17.2292 10.8958 16.7833 10.2333 16.2 9.65001C15.8667 9.86667 15.5167 10.0292 15.15 10.1375C14.7833 10.2458 14.4083 10.3 14.025 10.3C12.9917 10.3 12.0958 9.95834 11.3375 9.27501C10.5792 8.59167 10.1417 7.75001 10.025 6.75001C8.725 7.85001 7.72917 9.02084 7.0375 10.2625C6.34583 11.5042 6 12.75 6 14ZM12 15.3L10.575 16.7C10.3917 16.8833 10.25 17.0917 10.15 17.325C10.05 17.5583 10 17.8 10 18.05C10 18.5833 10.1958 19.0417 10.5875 19.425C10.9792 19.8083 11.45 20 12 20C12.55 20 13.0208 19.8083 13.4125 19.425C13.8042 19.0417 14 18.5833 14 18.05C14 17.7833 13.95 17.5375 13.85 17.3125C13.75 17.0875 13.6083 16.8833 13.425 16.7L12 15.3Z" fill="currentColor"/>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+                <div>
+                    <a href="" class="desktop6__iconTab">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <mask id="mask0_5_1269" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                                <rect width="24" height="24" fill="currentColor"/>
+                            </mask>
+                            <g mask="url(#mask0_5_1269)">
+                                <path d="M10.65 19.8L8.925 18.225C7.15833 16.6084 5.5625 15.0042 4.1375 13.4125C2.7125 11.8209 2 10.0667 2 8.15002C2 6.58336 2.525 5.27502 3.575 4.22502C4.625 3.17502 5.93333 2.65002 7.5 2.65002C8.38333 2.65002 9.21667 2.83752 10 3.21252C10.7833 3.58752 11.45 4.10002 12 4.75002C12.55 4.10002 13.2167 3.58752 14 3.21252C14.7833 2.83752 15.6167 2.65002 16.5 2.65002C18.0667 2.65002 19.375 3.17502 20.425 4.22502C21.475 5.27502 22 6.58336 22 8.15002C22 10.0667 21.2917 11.825 19.875 13.425C18.4583 15.025 16.85 16.6334 15.05 18.25L13.35 19.8C12.9667 20.1667 12.5167 20.35 12 20.35C11.4833 20.35 11.0333 20.1667 10.65 19.8ZM11.05 6.75002C10.5667 6.06669 10.05 5.54586 9.5 5.18752C8.95 4.82919 8.28333 4.65002 7.5 4.65002C6.5 4.65002 5.66667 4.98336 5 5.65002C4.33333 6.31669 4 7.15002 4 8.15002C4 9.01669 4.30833 9.93752 4.925 10.9125C5.54167 11.8875 6.27917 12.8334 7.1375 13.75C7.99583 14.6667 8.87917 15.525 9.7875 16.325C10.6958 17.125 11.4333 17.7834 12 18.3C12.5667 17.7834 13.3042 17.125 14.2125 16.325C15.1208 15.525 16.0042 14.6667 16.8625 13.75C17.7208 12.8334 18.4583 11.8875 19.075 10.9125C19.6917 9.93752 20 9.01669 20 8.15002C20 7.15002 19.6667 6.31669 19 5.65002C18.3333 4.98336 17.5 4.65002 16.5 4.65002C15.7167 4.65002 15.05 4.82919 14.5 5.18752C13.95 5.54586 13.4333 6.06669 12.95 6.75002C12.8333 6.91669 12.6917 7.04169 12.525 7.12502C12.3583 7.20836 12.1833 7.25002 12 7.25002C11.8167 7.25002 11.6417 7.20836 11.475 7.12502C11.3083 7.04169 11.1667 6.91669 11.05 6.75002Z" fill="currentColor"/>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="item__32-16 desktop5__labelTitle">Top like</div>
+</div>
+<div uk-toggle="cls: desktop5__container uk-container-expand-right uk-container-item-padding-remove-right; mode: media; media: @l" class="uk-container item__16-8">
+    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="center: false">
+        <ul class="uk-slider-items uk-grid uk-grid-match uk-grid-8" uk-grid>
+            <?php for ($i=1;$i<=9;$i++): ?>
+            <li class="uk-width uk-width-auto@s">
+                <div class="uk-card desktop5__card">
+                    <div class="uk-card-header uk-padding-small">
+                        <div class="uk-grid uk-flex-middle" uk-grid>
+                            <div class="uk-width-expand">
+                                <div class="desktop5__card__tag">#<?= $i ?></div>
+                            </div>
+                            <div class="uk-width-auto">
+                                <a href="" class="desktop5__card__icon">
+                                    12
+                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <mask id="mask0_5_1327" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="20" height="20">
+                                            <rect width="20" height="20" fill="#D9D9D9"/>
+                                        </mask>
+                                        <g mask="url(#mask0_5_1327)">
+                                            <path d="M8.87496 16.5L7.43746 15.1875C5.96524 13.8403 4.63538 12.5035 3.44788 11.1771C2.26038 9.85074 1.66663 8.38893 1.66663 6.79171C1.66663 5.48615 2.10413 4.39587 2.97913 3.52087C3.85413 2.64587 4.9444 2.20837 6.24996 2.20837C6.98607 2.20837 7.68051 2.36462 8.33329 2.67712C8.98607 2.98962 9.54163 3.41671 9.99996 3.95837C10.4583 3.41671 11.0138 2.98962 11.6666 2.67712C12.3194 2.36462 13.0138 2.20837 13.75 2.20837C15.0555 2.20837 16.1458 2.64587 17.0208 3.52087C17.8958 4.39587 18.3333 5.48615 18.3333 6.79171C18.3333 8.38893 17.743 9.85421 16.5625 11.1875C15.3819 12.5209 14.0416 13.8612 12.5416 15.2084L11.125 16.5C10.8055 16.8056 10.4305 16.9584 9.99996 16.9584C9.5694 16.9584 9.1944 16.8056 8.87496 16.5ZM9.20829 5.62504C8.80551 5.0556 8.37496 4.62157 7.91663 4.32296C7.45829 4.02435 6.90274 3.87504 6.24996 3.87504C5.41663 3.87504 4.72218 4.15282 4.16663 4.70837C3.61107 5.26393 3.33329 5.95837 3.33329 6.79171C3.33329 7.51393 3.59024 8.28129 4.10413 9.09379C4.61801 9.90629 5.2326 10.6945 5.94788 11.4584C6.66315 12.2223 7.39926 12.9375 8.15621 13.6042C8.91315 14.2709 9.52774 14.8195 9.99996 15.25C10.4722 14.8195 11.0868 14.2709 11.8437 13.6042C12.6007 12.9375 13.3368 12.2223 14.052 11.4584C14.7673 10.6945 15.3819 9.90629 15.8958 9.09379C16.4097 8.28129 16.6666 7.51393 16.6666 6.79171C16.6666 5.95837 16.3888 5.26393 15.8333 4.70837C15.2777 4.15282 14.5833 3.87504 13.75 3.87504C13.0972 3.87504 12.5416 4.02435 12.0833 4.32296C11.625 4.62157 11.1944 5.0556 10.7916 5.62504C10.6944 5.76393 10.5763 5.8681 10.4375 5.93754C10.2986 6.00699 10.1527 6.04171 9.99996 6.04171C9.84718 6.04171 9.70135 6.00699 9.56246 5.93754C9.42357 5.8681 9.30551 5.76393 9.20829 5.62504Z" fill="#D9D9D9"/>
+                                        </g>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-card-body uk-padding-remove uk-cover-container">
+                        <img src="https://picsum.photos/328/328/?random=<?= $i ?>" alt="" uk-cover="">
+                        <canvas width="328" height="328"></canvas>
+                    </div>
+                    <div class="uk-card-footer uk-padding-small">
+                        <div class="uk-grid uk-flex-middle" uk-grid>
+                            <div class="uk-width-expand">
+                                <div class="uk-grid uk-grid-8 uk-flex-middle" uk-grid>
+                                    <div class="uk-width-auto">
+                                        <div class="uk-cover-container uk-border-circle">
+                                            <img src="images/lemanhhai_anhthe.jpg" alt="" uk-cover="">
+                                            <canvas width="32" height="32"></canvas>
+                                        </div>
+                                    </div>
+                                    <div class="uk-width-expand">
+                                        <div class="desktop8__name">User Name</div>
+                                        <div class="desktop8__txtSmall">1.2m followers</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="uk-width-auto">
+                                <a href="" class="desktop8__btnFollow uk-active uk-button uk-button-default uk-button-small uk-border-pill">Follow</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <?php endfor; ?>
+        </ul>
+
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+    </div>
+</div>
+<div class="uk-container item__32-16">
+    <div class="desktop5__labelTitle">Top Share</div>
+</div>
+<div uk-toggle="cls: desktop5__container uk-container-expand-right uk-container-item-padding-remove-right; mode: media; media: @l" class="uk-container item__16-8">
+    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="center: false">
+        <ul class="uk-slider-items uk-grid uk-grid-match uk-grid-8" uk-grid>
+            <?php for ($j=1;$j<=9;$j++): ?>
+                <li class="uk-width uk-width-auto@s">
+                    <div class="uk-card desktop5__card">
+                        <div class="uk-card-header uk-padding-small">
+                            <div class="uk-grid uk-flex-middle" uk-grid>
+                                <div class="uk-width-expand">
+                                    <div class="desktop5__card__tag">#<?= $j ?></div>
+                                </div>
+                                <div class="uk-width-auto">
+                                    <a href="" class="desktop5__card__icon">
+                                        12
+                                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <mask id="mask0_5_1793" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="20" height="20">
+                                                <rect width="20" height="20" fill="#D9D9D9"/>
+                                            </mask>
+                                            <g mask="url(#mask0_5_1793)">
+                                                <path d="M9.99998 13.3333C9.76387 13.3333 9.56595 13.2535 9.40623 13.0938C9.24651 12.934 9.16665 12.7361 9.16665 12.5V4.02083L8.41665 4.77083C8.26387 4.92361 8.0729 5 7.84373 5C7.61456 5 7.41665 4.91667 7.24998 4.75C7.0972 4.58333 7.02081 4.38542 7.02081 4.15625C7.02081 3.92708 7.0972 3.73611 7.24998 3.58333L9.41665 1.41667C9.48609 1.34722 9.5729 1.29167 9.67706 1.25C9.78123 1.20833 9.88887 1.1875 9.99998 1.1875C10.1111 1.1875 10.2187 1.20833 10.3229 1.25C10.4271 1.29167 10.5139 1.34722 10.5833 1.41667L12.75 3.58333C12.9166 3.75 13 3.95139 13 4.1875C13 4.42361 12.9166 4.61806 12.75 4.77083C12.5833 4.92361 12.3854 5 12.1562 5C11.9271 5 11.7361 4.92361 11.5833 4.77083L10.8333 4.02083V12.5C10.8333 12.7361 10.7535 12.934 10.5937 13.0938C10.434 13.2535 10.2361 13.3333 9.99998 13.3333ZM4.99998 19.1667C4.54165 19.1667 4.14929 19.0035 3.8229 18.6771C3.49651 18.3507 3.33331 17.9583 3.33331 17.5V8.33333C3.33331 7.875 3.49651 7.48264 3.8229 7.15625C4.14929 6.82986 4.54165 6.66667 4.99998 6.66667H6.66665C6.90276 6.66667 7.10067 6.74653 7.2604 6.90625C7.42012 7.06597 7.49998 7.26389 7.49998 7.5C7.49998 7.73611 7.42012 7.93403 7.2604 8.09375C7.10067 8.25347 6.90276 8.33333 6.66665 8.33333H4.99998V17.5H15V8.33333H13.3333C13.0972 8.33333 12.8993 8.25347 12.7396 8.09375C12.5798 7.93403 12.5 7.73611 12.5 7.5C12.5 7.26389 12.5798 7.06597 12.7396 6.90625C12.8993 6.74653 13.0972 6.66667 13.3333 6.66667H15C15.4583 6.66667 15.8507 6.82986 16.1771 7.15625C16.5035 7.48264 16.6666 7.875 16.6666 8.33333V17.5C16.6666 17.9583 16.5035 18.3507 16.1771 18.6771C15.8507 19.0035 15.4583 19.1667 15 19.1667H4.99998Z" fill="#D9D9D9"/>
+                                            </g>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-card-body uk-padding-remove uk-cover-container">
+                            <img src="https://picsum.photos/328/328/?random=<?= $j+9 ?>" alt="" uk-cover="">
+                            <canvas width="328" height="328"></canvas>
+                        </div>
+                        <div class="uk-card-footer uk-padding-small">
+                            <div class="uk-grid uk-flex-middle" uk-grid>
+                                <div class="uk-width-expand">
+                                    <div class="uk-grid uk-grid-8 uk-flex-middle" uk-grid>
+                                        <div class="uk-width-auto">
+                                            <div class="uk-cover-container uk-border-circle">
+                                                <img src="images/lemanhhai_anhthe.jpg" alt="" uk-cover="">
+                                                <canvas width="32" height="32"></canvas>
+                                            </div>
+                                        </div>
+                                        <div class="uk-width-expand">
+                                            <div class="desktop8__name">User Name</div>
+                                            <div class="desktop8__txtSmall">1.2m followers</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="uk-width-auto">
+                                    <a href="" class="desktop8__btnFollow uk-active uk-button uk-button-default uk-button-small uk-border-pill">Follow</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            <?php endfor; ?>
+        </ul>
+
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
